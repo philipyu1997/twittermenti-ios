20dab32a4f7065a7816df4f1cd3e1e86bf1ee694
Philip Yu <pyu3@mail.sfsu.edu>
2019-07-18 19:56:52 -0700

    Add Cocoapods and sentiment classification

--- Twittermenti/Controller/ViewController.swift
+++ Twittermenti/Controller/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import SwifteriOS
+import CoreML
+import SwiftyJSON
 
 class ViewController: UIViewController {
     
@@ -16,22 +18,70 @@ class ViewController: UIViewController {
     @IBOutlet weak var sentimentLabel: UILabel!
     
     let swifter = Swifter(consumerKey: "INSERT TWITTER API KEY", consumerSecret: "INSERT TWITTER SECRET API KEY")
-
+    let sentimentClassifier = TweetSentimentClassifier()
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
-        swifter.searchTweet(using: "@Apple", success: { (results, metadata) in
-            print(results)
-        }) { (error) in
-            print("There was an error with the Twitter API Request, \(error)")
+    }
+    
+    @IBAction func predictPressed(_ sender: Any) {
+        
+        if let searchText = textField.text {
+            swifter.searchTweet(using: searchText, lang: "en", count: 100, tweetMode: .extended, success: { (results, metadata) in
+                
+                var tweets = [TweetSentimentClassifierInput]()
+                
+                for i in 0..<100 {
+                    if let tweet = results[i]["full_text"].string {
+                        let tweetForClassification = TweetSentimentClassifierInput(text: tweet)
+                        
+                        tweets.append(tweetForClassification)
+                        print("\(i) \(tweet)")
+                    }
+                }
+                
+                do {
+                    let predictions = try self.sentimentClassifier.predictions(inputs: tweets)
+                    var sentimentScore = 0
+                    
+                    for prediction in predictions {
+                        let sentiment = prediction.label
+                        
+                        if sentiment == "Pos" {
+                            sentimentScore += 1
+                        } else if sentiment == "Neg" {
+                            sentimentScore -= 1
+                        }
+                    }
+                    
+                    if sentimentScore > 20 {
+                        self.sentimentLabel.text = "😍"
+                    } else if sentimentScore > 10 {
+                        self.sentimentLabel.text = "😃"
+                    } else if sentimentScore > 0 {
+                        self.sentimentLabel.text = "🙂"
+                    } else if sentimentScore == 0 {
+                        self.sentimentLabel.text = "😐"
+                    } else if sentimentScore > -10 {
+                        self.sentimentLabel.text = "😕"
+                    } else if sentimentScore > -20 {
+                        self.sentimentLabel.text = "😡"
+                    } else {
+                        self.sentimentLabel.text = "🤮"
+                    }
+                    
+                    print("sentimentScore: \(sentimentScore)")
+                } catch {
+                    print("There was an error with making a prediciton, \(error)")
+                }
+                
+            }) { (error) in
+                print("There was an error with the Twitter API Request, \(error)")
+            }
         }
         
-    }
-
-    @IBAction func predictPressed(_ sender: Any) {
-    
-    
     }
     
 }
