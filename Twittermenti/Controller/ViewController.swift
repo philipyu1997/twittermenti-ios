bacbc7fb1763461b7dea7306c9e4b42711181639
Philip Yu <pyu3@mail.sfsu.edu>
2019-07-18 17:57:27 -0700

    Set up Swifter framework

--- Twittermenti/Controller/ViewController.swift
+++ Twittermenti/Controller/ViewController.swift
@@ -7,16 +7,25 @@
 //
 
 import UIKit
+import SwifteriOS
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var sentimentLabel: UILabel!
+    
+    let swifter = Swifter(consumerKey: "INSERT TWITTER API KEY", consumerSecret: "INSERT TWITTER SECRET API KEY")
 
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        
+        swifter.searchTweet(using: "@Apple", success: { (results, metadata) in
+            print(results)
+        }) { (error) in
+            print("There was an error with the Twitter API Request, \(error)")
+        }
         
     }
 
