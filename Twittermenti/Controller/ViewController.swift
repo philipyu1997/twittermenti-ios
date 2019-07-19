8a8e6500e1a22c8c8e2db663b8fd39815bfd560e
Philip Yu <pyu3@mail.sfsu.edu>
2019-07-18 17:24:06 -0700

    Add Swifter framework

--- Twittermenti/Controller/ViewController.swift
+++ Twittermenti/Controller/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var sentimentLabel: UILabel!
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
     }
